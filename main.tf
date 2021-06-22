6f6feaae52dca9c7303172ea8208f47346822497
Masa Ito <masa@hashicorp.com>
2021-06-22 15:10:49 +0900

    Fixed CIDR

--- main.tf
+++ main.tf
@@ -46,21 +46,21 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["121.6.14.14/32", "0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["121.6.14.14/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["121.6.14.14/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
